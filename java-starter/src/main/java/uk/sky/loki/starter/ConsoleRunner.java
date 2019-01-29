a47402930e4a42e55ef953cd4de2557759147fd1
Tomasz Juszczyk <tomasz.juszczyk@sky.uk>
2019-01-29 13:19:07 +0000

    [kata1] template for kata1

---
+++ java-starter/src/main/java/uk/sky/loki/starter/ConsoleRunner.java
@@ -0,0 +1,12 @@
+package uk.sky.loki.starter;
+
+import java.io.IOException;
+
+public class ConsoleRunner {
+
+    public static void main(String[] args) throws IOException {
+
+    }
+
+
+}
---
+++ java-starter/src/test/java/JUnitTest.java
@@ -0,0 +1,11 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class JUnitTest {
+
+
+    @Test
+    public void testIt(){
+        Assertions.assertThat(true).isTrue();
+    }
+}
